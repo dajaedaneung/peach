e4611cef80fde7fe495527c11cd1e61d826afb1d
Thinker <102664109+YJH2848@users.noreply.github.com>
2023-01-12 10:26:13 +0900

    Update strandtest.c
    Success : json연결 후 가져오기 성공

--- strandtest.c
+++ strandtest.c
@@ -1,27 +1,15 @@
+#include <ESP8266HTTPClient.h>
 #include <Adafruit_NeoPixel.h>
-#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+
 
 const char* wifi = "bssm_free";
 const char* password = "bssm_free";
+const String url = "http://10.150.150.191:8080/density/now?camera=1";
+WiFiClient wifiClient;
 
 
 
-//고정적으로 사용할 내용을 미리 선언
-#define LED_PIN1 D5     //네오픽셀에 신호를 줄 핀번호
-#define LED_PIN2 D6     //네오픽셀에 신호를 줄 핀번호
-#define LED_PIN3 D7     //네오픽셀에 신호를 줄 핀번호
-// #define LED_PIN4 D8
-   //아두이노에 연결된 네오픽셀의 개수
-   //red : 3
-   //yellow : 2
-   //blue : 1
-int a = 3;
-int b = 1;
-int c = 2; // or 2 or 3
-Adafruit_NeoPixel strip1(11, LED_PIN1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2(11, LED_PIN2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip3(11, LED_PIN3, NEO_GRB + NEO_KHZ800);
 // Adafruit_NeoPixel strip(LED_COUNT, LED_PIN3, NEO_GRB + NEO_KHZ800);
 // Adafruit_NeoPixel strip(LED_COUNT, LED_PIN4 NEO_GRB + NEO_KHZ800);
 // 라이브러리에서 네오픽셀 객체를 선언
@@ -39,106 +27,57 @@ Adafruit_NeoPixel strip3(11, LED_PIN3, NEO_GRB + NEO_KHZ800);
      */
     
 void setup() {
-  Serial.begin(9600);
-   
-  strip1.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip1.show();            // 네오픽셀에 빛을 출력하기 위한 것인데 여기서는 모든 네오픽셀을 OFF하기 위해서 사용한다.
-  strip1.setBrightness(10);
+  Serial.begin(115200);
+  delay(4000);   //Delay needed before calling the WiFi.begin
   
-  strip2.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip2.show();            // 네오픽셀에 빛을 출력하기 위한 것인데 여기서는 모든 네오픽셀을 OFF하기 위해서 사용한다.
-  strip2.setBrightness(10); // 네오픽셀의 밝기 설정(최대 255까지 가능)
+  WiFi.begin(wifi, password); 
   
-  strip3.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip3.show();            // 네오픽셀에 빛을 출력하기 위한 것인데 여기서는 모든 네오픽셀을 OFF하기 위해서 사용한다.
-  strip3.setBrightness(10); // 네오픽셀의 밝기 설정(최대 255까지 가능)
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println("Connected to the WiFi network");
 }
 
-void loop() {
-  if(code == "NICE"){
-    for(int j = 0; j < 12; j++){
-        strip1.setPixelColor(j, strip1.Color(0, 255, 0));
-    }
-    strip1.show();
-  }
-  else if(code == "NORMAL"){
-    for(int j = 0; j < 12; j++){
-        strip1.setPixelColor(j, strip1.Color(255, 255, 0));
-    }
-    strip1.show();
-  }
-  else if(code == "WARNING"){
-    for(int j = 0; j < 12; j++){
-        strip1.setPixelColor(j, strip1.Color(255, 0, 0));
-    }
-    strip1.show();
-  }
+void loop(){
+  if (WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.begin(wifiClient, url);
+    http.addHeader("Content-Type", "application/json");
 
-
-  if(code == "NORMAL"){
-    for(int j = 0; j < 12; j++){
-        strip2.setPixelColor(j, strip2.Color(255, 255, 0));
+    int httpCode = http.GET(); 
+      //Send the actual POST request
+    if (httpCode > 0) {
+ 
+        // String vom Webseiteninhalt speichern
+        String payload = http.getString();
+        Serial.println(httpCode);
+        // Hier kann mit dem Wert weitergearbeitet werden
+         // ist aber nicht unbedingt notwendig
+        Serial.println("불러온 JSON파일 값 : " + payload);
+        Serial.print("테스트테스트");
+        
+        
+      
+  }else{
+      // Falls HTTP-Error
+      Serial.printf("HTTP-Error: ", http.errorToString(httpCode).c_str());
     }
-    strip2.show();
-  }else if(code == "NICE"){
-    for(int j = 0; j < 12; j++){
-        strip2.setPixelColor(j, strip2.Color(0, 255, 0));
-    }
-    strip2.show();
-  }else if(code == "WARNING"){
-    for(int j = 0; j < 12; j++){
-        strip2.setPixelColor(j, strip2.Color(255, 0, 0));
-    }
-    strip2.show();
-  }
-
-
-
-  if(code == "WARNING"){
-    for(int j = 0; j < 12; j++){
-        strip3.setPixelColor(j, strip3.Color(255, 0, 0));
-    }
-    strip3.show();
-  }else if(code == "NICE"){
-    for(int j = 0; j < 12; j++){
-        strip3.setPixelColor(j, strip3.Color(0, 255, 0));
-    }
-    strip3.show();
-  }else if(code == "NORMAL"){
-    for(int j = 0; j < 12; j++){
-        strip3.setPixelColor(j, strip3.Color(255, 255, 0));
-    }
-    strip3.show();
+ 
+    // Wenn alles abgeschlossen ist, wird die Verbindung wieder beendet
+    http.end();
+    
+  }else {
+    Serial.printf("HTTP-Verbindung konnte nicht hergestellt werden!");
   }
   
-  // 스트립 길이를 따라서 설정된 색으로 채운다.
-  // strip.Color(Red,   Green,   Blue) 스트립의 색상을 RGB순서대로 세팅해준다.각RGB마다 0~255까지 설정가능
-  // colorWipe(스트립 색상, 딜레이 시간)
+  delay(10000);  //Send a request every 10 seconds
   
-  // colorWipe(strip.Color(  80,   188, 223), 0); // Blue
-  
-  // colorWipe(strip.Color(  0,   0, 255), 60);
+}
 
-  // colorWipe(strip.Color(  0,   0, 0), 0);
-  
-  // colorWipe(strip.Color(  0,   0, 0), 80); // Blue
 
-  // 극장간판에 달린 전구가 빛나는것과 유사한 효과
-  // theaterChase(스트립 색상, 딜레이 시간)
-  
-  
-  //해당 함수들은 밑에 구현되어있다.
-}s
-// void colorWipe(uint32_t color, int wait) {
-  
-//   for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
-//     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-//     strip.show();                          //  Update strip to match
-//     delay(wait);  
-//              //  Pause for a moment
-//   }
 
-// }
 
 
 
